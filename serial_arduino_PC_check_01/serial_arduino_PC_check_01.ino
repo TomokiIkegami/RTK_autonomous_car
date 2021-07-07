b7b58bf8a354702ed6ade425c3951d3a76822e49
hwataru <s173134@edu.asahikawa-nct.ac.jp>
2021-07-07 09:59:48 +0900

    負の値もPCからマイコンに送信できました！

--- serial_arduino_PC_check_01/serial_arduino_PC_check_01.ino
+++ serial_arduino_PC_check_01/serial_arduino_PC_check_01.ino
@@ -1,57 +1,40 @@
-////////////////////////////////////////////
-//  arduinoとpythonのシリアル通信のチェック
-//  2021.5.17
-/////////////////////////////////////////////////////////////
+/*/////////////////////////////////////////////////////////
+PCから整数を読み取って，整数をPCに返すプログラムです．
+受け取った整数は符号なし(0~255)なので，符号付(-127~128)の値に変換しています．
+/////////////////////////////////////////////////////////*/
+
+//#include<LiquidCrystal_I2C.h>
+//LiquidCrystal_I2C lcd(0x27,16,2);
 
 void setup() {
   Serial.begin(115200);// arduino IDEモニタ用
   while (!Serial); 
   delay(500);
+//  lcd.init();
+//  lcd.backlight();
+//  lcd.setCursor(0,0);
+//  lcd.print("Good morning!");
+//  lcd.setCursor(0,1);
+//  lcd.print("Are you happy?");
 }
 
 
 void loop() {
+//  lcd.init(); //ディスプレイの表示を初期化
+//  lcd.setCursor(0,0);
+//  lcd.print("Receiving Dis");
 
-  byte cc;
-  
   while(1){
     if(Serial.available()>0){
-      cc = (byte)Serial.read();
-      //int cc = Serial.read();
-      if(cc == '-'){ // 0x41 == 'A'
-        //Serial.print(cc);   
-        Serial.write(cc);
-        break;
-      }else{
-        Serial.write('+');
-        break;
-        }
-    }
-  }
-
-  
-    //delay(1000);
-  while(1){
-    if(Serial.available()>0){
-      byte dd = (byte)Serial.read();
-      if(cc == '-'){
-        delay(1000);
-        Serial.write('X');
-        //byte dd = (byte)Serial.read();
-        //Serial.write(dd);
-        break;
-        
-        }else{
-        Serial.write(dd);
-        break;
-          }
-
-      //Serial.write(dd);
-      //break;
-      
+     byte cc = (byte)Serial.read(); //PCから，距離の値を読み込みます(符号なし0~255)
+      char real_d=(char)cc; //値を符号付きに変換 (-128~127)←計算にはこの値を使用すればよい．
       //if(cc == 'B'){ 
-        //Serial.write(cc);   
-        //break;
+        Serial.write((byte)cc); //値をPCに送信(符号なし0~255)
+//        lcd.setCursor(0,1);
+//        lcd.print("D=");
+//        lcd.setCursor(2,1);
+//        lcd.print((int)real_d);
+        break;
       //}
     }
   }
--- serial_test.py
+++ serial_test.py
@@ -7,7 +7,7 @@ ser = serial.Serial('COM6',115200,timeout = 0.1)	# シリアル通信開始
 
 
 while True:
-    time.sleep(2.0)
+    time.sleep(1.0)
     # while True:
     #         ser.write(b'A') # 'A' == 0x41
     #         #print("arduinoにAを送りました2021.5.17")   
@@ -16,63 +16,27 @@ while True:
     #         if c == b'A':
     #             print(c)
     #             break
+
     
-    d=-100
+
+    d=-128 #ずれ量の値
+
+    #ずれ量が負の場合は，符号なしの値に変換
+    if d<0: 
+        d=256-abs(d)
+
+
+
     #hex_d=hex(d)
     #print(hex_d)
+    bina_d=bytes([d]) #距離の値をバイト型に変換
 
-    if d>=0:
-        
+    ser.write(bina_d) # 'A' == 0x41 #距離の値をバイト型でマイコンに送信
+    #print("arduinoにBを送りました2021.5.17")   
+    time.sleep(0.1)
+    c = ser.read() #マイコンから値を読み込む
 
-        while True:
-                ser.write(b'+') # 'A' == 0x41
-                #print("arduinoにAを送りました2021.5.17")   
-                time.sleep(0.1)
-                c = ser.read()
-                if c == b'+':
-                    print(c)
-                    break
-
-        bina_d=bytes([d])
-
-        ser.write(bina_d) # 'A' == 0x41
-        #print("arduinoにBを送りました2021.5.17")   
-        time.sleep(0.1)
-
-        c = ser.read()
-
-
-        print("byte型:{0}".format(c))
-        xx=int.from_bytes(c, 'big')
-        print('byte→int:{0}'.format(xx))
-
-    else:
-
-        #マイナス符号をマイコンに送信
-        #ser.write(b"-")
-
-        while True:
-                ser.write(b'-') # 'A' == 0x41
-                #print("arduinoにAを送りました2021.5.17")   
-                time.sleep(0.1)
-                c = ser.read()
-                if c == b'-':
-                    print(c)
-                    break
-
-
-        #距離の絶対値をマイコンに送信
-        bina_d=bytes([abs(d)])
-        ser.write(bina_d) # 'A' == 0x41
-
-        #マイコンから値を読み込む
-        c = ser.read()
-
-        #マイコンから読み込んできた値を表示する．
-        print("byte型:{0}".format(c))
-        xx=int.from_bytes(c, 'big')
-        print('byte→int:{0}'.format(xx))
-
-        
-
-
+    #マイコンから返ってきた値を表示している．
+    print("byte型:{0}".format(c))   
+    xx=int.from_bytes(c, 'big')
+    print('byte→int:{0}'.format(xx))
