167d6a8b23b056d01d792fcb8d6bdd30eecd943f
hwataru <s173134@edu.asahikawa-nct.ac.jp>
2021-07-08 10:33:39 +0900

    超音波センサプログラムに，角度情報を追加しました．

--- Ultrasonic_Wall/Ultrasonic_Wall.ino
+++ Ultrasonic_Wall/Ultrasonic_Wall.ino
@@ -26,12 +26,19 @@
 #define MOTOR_3  (34)  // green
 #define MOTOR_4  (35)  // blue
 
+#define Addr_Mag 0x13
+
 #define SW  (36)
 #define MOTOR_STEPS (2048) // 出力軸1回転ステップ数:2048（2相磁励）
      // カタログ値です 1ステップ 0.17578度、10ステップ 1.7578度
 // Relay pin //
 #define RELAY1 (30)
 #define RELAY2 (31)
+
+// センサーの値を保存するグローバル関数
+int   xMag  = 0;
+int   yMag  = 0;
+int   zMag  = 0;
 
 /* Ultroasonic Sensor setting */
 int trig1 = 3;// 出力ピン
@@ -79,7 +86,7 @@ int ii = 0;
 const double One_step_angle = 360.0/MOTOR_STEPS;
 const int User_steps = 57;// 回転してほしいステップ数 57で10度です
 double v_angle = 0;
-/*double cal_x = 0;
+double cal_x = 0;
 double cal_y = 0;
 double cal_x_real = 0;
 double cal_y_real = 0;
@@ -88,13 +95,13 @@ double ave_cal_y = 0;
 double cal_x_north = 0;
 double cal_y_north = 0;
 double rd = 0;
-double rd_north = 0;*/
+double rd_north = 0;
 //double seihen = 0.16872;
 //磁北の状態 旭川市春光台では西偏9度40分 = 9.667度 = 0.16872rad
 // しかし電子コンパスは磁北を指さないので西偏値を引く必要は無い
-//int count = 0;
+int count = 0;
 int rot_dir = 0;
-//int hensa = 0;
+int hensa = 0;
 //ステッピングモータ回転方向 rot_dir=0 は上から見て反時計回り
 //ステッピングモータ回転方向 rot_dir=1 は上から見て時計回り
 //ステッピングモータ回転方向 rot_dir=4 は停止
@@ -166,6 +173,10 @@ double DR=17; //DR:Dual Rate ，舵角のこと。中心から片側に操舵し
 int dest = 17;//ロータリーエンコーダとギヤ取り付け部がソフトなのでずれるが７にする 8から17.5に変更
 double sita;
 double delta_l;
+
+
+
+
 
 void setup() {
   Serial.begin(115200);// arduino IDEモニタ用
@@ -281,7 +292,7 @@ void setup() {
       stopMotor(); 
       delay(500);
       rot_dir = 5;
-      /*Serial.println("平均値");  
+      Serial.println("平均値");  
       ave_cal_x = cal_x/count;//楕円中心のｘ座標 
       Serial.println(ave_cal_x);
       ave_cal_y = cal_y/count;//楕円中心のｙ座標
@@ -291,7 +302,7 @@ void setup() {
       cal_x_north = cal_x_real -ave_cal_x; 
       cal_y_north = cal_y_real -ave_cal_y; 
       rd_north = getDirection(cal_x_north, cal_y_north);// y軸が角度の基準としている
-      */
+     
       // rd_north は台車を置いた位置での磁北の方向
     }
     delay(100);  
@@ -373,7 +384,7 @@ void loop() {
   ////////////////////////
   // 前輪操舵制御 //
   ////////////////////////  
-  int stop_f = Feed_Back(sita, delta_l);
+  int stop_f = Feed_Back(sita, delta_l/100); //delta_l/100 → 単位を[cm]から[m]にするため．
   if(stop_f == 1) exit(0);
   delay(300); 
 }
@@ -412,28 +423,163 @@ void Ultrasonic(void){
     // Distance2 単位 cm
     Distance_2 = Distance_2 * 340.0 * 100.0 / 1000000.0; // ultrasonic speed is 340m/s = 34000cm/s = 0.034cm/us 
   }
-    
+
+
+  ////////////////////////
+  // コンパスデータ受信です //
+  ////////////////////////  
+  compass_Rawdata_Real();
+  cal_x_real = cal_x_real -ave_cal_x; 
+  cal_y_real = cal_y_real -ave_cal_y;   
+  rd = getDirection(cal_x_real, cal_y_real);
+  
   // double atan(double x) = －π/2 から π/2
   // 方位 radian
-  sita = atan( (Distance_1 - Distance_2) / LL );
+  //sita = atan( (Distance_1 - Distance_2) / LL );
+  sita = rd_north - rd; //角度の値を磁気コンパスのものに置き換え
+
+
   // 基準位置からの距離 [m] 
   //delta_l = (Distance_1 + Distance_2)/2.0/100.0 - Distance_c;// [m]にした  
 
   //PCからずれ量を取得
-  /**次回はこの続きから**/
+
+
+
 
   while(1){
     if(Serial.available()>0){
       byte cc=(byte)Serial.read();
-      char real_d=(char)cc;
-
-      
+      char delta_l=(char)cc; //経路からのずれ量[cm]
+      break;
       }
     
     }
 
   
 }
+
+/*磁気コンパス関連の関数*/
+
+// コンパスの生データを取得する
+void compass_Rawdata(){
+  //BMX055 磁気の読み取り
+  BMX055_Mag();
+  Serial.print("xMag,yMag,zMag,rot_dir,count :  ");
+  Serial.print(xMag); cal_x += (double)xMag;
+  Serial.print(",");
+  Serial.print(yMag); cal_y += (double)yMag;
+  Serial.print(",");
+  Serial.print(zMag);
+  Serial.print(","); 
+  Serial.print(rot_dir);
+  Serial.print(","); 
+  Serial.print(count);
+  Serial.print(","); 
+  Serial.println();
+}
+
+void compass_Rawdata_Real(){
+  //BMX055 磁気の読み取り
+  BMX055_Mag();
+  cal_x_real= (double)xMag;
+  cal_y_real= (double)yMag;
+}
+
+//** 角度を求める 出力は Radian **//
+double getDirection(double x, double y){
+  double dir;
+  if(y == 0) dir = 0;
+  else if(x == 0 && y > 0) dir = PI/2.0;
+  else if(x == 0 && y < 0) dir = -PI/2.0;
+  else {
+    dir = atan(y/x);// 戻り値 -pi/2 から +pi/2
+    if(x < 0 && y >= 0) dir = dir + PI;
+    else if(x < 0 && y <= 0) dir = dir - PI;
+  }
+  // 結果として　-PI < dir < PI
+  return dir;
+}
+
+
+//=====================================================================================//
+void BMX055_Init()
+{
+//BMX055 初期化
+//------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4B);  // Select Mag register
+  Wire.write(0x83);  // Soft reset
+  Wire.endTransmission();
+  delay(100);
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4C);  // Select Mag register
+  Wire.write(0x00);  // Normal Mode, ODR = 10 Hz
+  Wire.endTransmission();
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4E);  // Select Mag register
+  Wire.write(0x84);  // X, Y, Z-Axis enabled
+  Wire.endTransmission();
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x51);  // Select Mag register
+  Wire.write(0x04);  // No. of Repetitions for X-Y Axis = 9
+  Wire.endTransmission();
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x52);  // Select Mag register
+  Wire.write(0x0F);  // No. of Repetitions for Z-Axis = 15
+  Wire.endTransmission();
+}
+
+//=====================================================================================//
+void BMX055_Mag()
+{
+   //BMX055 磁気の読み取り
+ unsigned int data[6];
+
+  for (int i = 0; i < 6; i++)
+  {
+    // Start I2C Transmission
+    Wire.beginTransmission(Addr_Mag);
+    // Select data register
+    Wire.write((66 + i));
+    // Stop I2C Transmission
+    Wire.endTransmission();
+
+    // Request 1 byte of data
+    Wire.requestFrom(Addr_Mag, 1);
+
+    // Read 6 bytes of data
+    // xMag lsb, xMag msb, yMag lsb, yMag msb, zMag lsb, zMag msb
+    if (Wire.available() == 1)
+    {
+      data[i] = Wire.read();
+    }
+  }
+
+  // Convert the data
+  xMag = ((data[1] * 256) + (data[0] & 0xF8)) / 8;
+  if (xMag > 4095)
+  {
+    xMag -= 8192;
+  }
+  yMag = ((data[3] * 256) + (data[2] & 0xF8)) / 8;
+  if (yMag > 4095)
+  {
+    yMag -= 8192;
+  }
+  zMag = ((data[5] * 256) + (data[4] & 0xFE)) / 2;
+  if (zMag > 16383)
+  {
+    zMag -= 32768;
+  }
+}
+
+
+
 
 // モーターへの電流を止める
 void stopMotor() {
